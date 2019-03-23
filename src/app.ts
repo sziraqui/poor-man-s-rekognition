50470525d98fdf7bf14f7704adcd4260c5b8ffc2
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-03-24 03:36:10 +0530

    add static web ui

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,24 @@
 import * as express from "express";
 import * as logger from "morgan";
 import * as bodyParser from "body-parser";
+import * as path from "path";
+import * as showError from "http-errors";
 
 import { indexRouter } from './routes/index';
 import { FaceDetection, FaceVerification } from "./ai-bridge/face-analytics";
 import { FaceDetectionService } from './services/detect-faces';
 import { CompareFacesService } from "./services/compare-faces";
 
+const createError = require("http-errors").createError;
 let initAI = async () => {
     await FaceDetection.getInstance();
     await FaceVerification.getInstance();
 }
 
-initAI()
-    .then(() => console.log('AI initialized'))
-    .catch(err => console.log(`Failed to initialise AI: ${err}`));
-    
+//initAI()
+//    .then(() => console.log('AI initialized'))
+//    .catch(err => console.log(`Failed to initialise AI: ${err}`));
+//    
 const app:express.Application = express();
 
 app.use(logger('dev'));
@@ -24,8 +27,27 @@ app.use(logger('dev'));
  */
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+/** Web demo static files */
+app.set('views', path.join(__dirname, '..', 'web-demo', 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, '..', 'web-demo', 'public')));
 /* Add web ui endpoints */
 app.use('/', indexRouter);
+/** Error handler */
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(showError(404));
+});
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {}; 
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 /** API endpoints */
 app.use('/api/face-detection', FaceDetectionService);
 app.use('/api/compare-faces', CompareFacesService);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,16 @@ let router:Router = Router();
 
 /* Check if server is running */
 router.get('/', function(req:Request, res:Response) {
-  res.send({'result': 'Server OK'});
+    res.send({'result': 'Server OK'});
 });
+
+router.get('/face-detection-demo', function(req:Request, res:Response) {
+    res.render('face-detection-demo', {title:"Poor Man's Rekognition", navtitle: "DetectFace API Demo"});
+});
+
+router.get('/face-compare-demo', function(req:Request, res:Response) {
+    res.render('face-compare-demo', {title:"Poor Man's Rekognition", navtitle: "CompareFace API Demo"});
+});
+
 
 export const indexRouter:Router = router;
